1bc523ed0db2257e8fbfa0eb811f56433c0a0ef7
KaiCYu <kaichyu@gmail.com>
2017-09-05 21:47:56 -0700

    added expiration date picker

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button} from '../../node_modules/react-toolbox/lib/button';
 import { Input } from '../../node_modules/react-toolbox/lib/input';
+import { DatePicker } from 'react-toolbox/lib/date_picker';
 
 import $ from 'jquery';
 
@@ -17,6 +18,7 @@ class App extends React.Component {
   }
 
   handleChange(name, value){
+    // console.log('name', name, 'value', value);
     this.setState({ [name]: value });
   };
 
@@ -26,8 +28,16 @@ class App extends React.Component {
       <h1>Tovia's Enigma</h1>
       
       <section> 
-        <Input type='text' label='Name' name='name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')} maxLength={16 } />
+        <Input type='text' label='Name' name='name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')} maxLength={16} />
         <Input type='text' label='Message' name={'message'} value={this.state.message} onChange={this.handleChange.bind(this, 'message')} maxLength={ 120 }></Input>
+
+        <DatePicker
+          label='Expiration date'
+          minDate={new Date()}
+          onChange={this.handleChange.bind(this, 'expireDate')}
+          value={this.state.expireDate}
+          sundayFirstDayOfWeek
+        />
         <Button label='Hello World!' />,
       </section>
 
